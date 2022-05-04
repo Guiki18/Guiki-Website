0f91f965e1519a9f607cdd0b95350758e30fb742
FoXZillaPT17 <>
2022-05-04 21:40:52 +0000

    Added a rateLimit Package

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
+const rateLimit = require('express-rate-limit');
+
 app.use(express.json());
 app.use(express.static("express"));
 
-app.use('/', function(req,res){
+const rateLimitProtetor = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	standardHeaders: true,
+	legacyHeaders: false,
+})
+
+app.use('/', rateLimitProtetor, function(req,res){
     res.sendFile(path.join(__dirname+'/web/'));
 });
 
 const server = http.createServer(app);
 const port = 3000;
+
 server.listen(port);
 console.debug('Server listening on port ' + port);
