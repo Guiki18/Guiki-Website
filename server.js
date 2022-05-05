55e8c8e171f2567172f8e8a6712040b1e535c7d5
Guiki18 <foxzillapt17@gmail.com>
2022-05-05 22:49:19 +0100

    Upgrades

--- server.js
+++ server.js
@@ -2,24 +2,39 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
-const rateLimit = require('express-rate-limit');
+const moment = require('moment');
+require('better-logging')(console, {
+	format: ctx => `${ctx.time24} ${ctx.date} ${ctx.type} | ${ctx.msg}`,
+	saveToFile: `${__dirname}/logs/${moment().format('L').replaceAll("/", "-")}.log`,
+	logLevels: {
+		debug: true,
+	}
+});
+const { lookup } = require('geoip-lite');
 
 app.use(express.json());
 app.use(express.static("express"));
 
-const rateLimitProtetor = rateLimit({
-	windowMs: 15 * 60 * 1000,
-	max: 100,
-	standardHeaders: true,
-	legacyHeaders: false,
-})
+app.use('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/web/'));
+	const ip = req.socket.remoteAddress;
+	const lookupIp = lookup(ip);
 
-app.use('/', rateLimitProtetor, function(req,res){
-    res.sendFile(path.join(__dirname+'/web/'));
+	const jsonRequestInformation = {
+		"method": req.method,
+		"url": req.url,
+		"ip": {
+			"ip": ip,
+			"lookupIp": lookupIp
+		},
+
+	}
+
+	console.log("Received a Request : \n" + JSON.stringify(jsonRequestInformation, null, '\t'));
 });
 
+console.debug("Starting server...");
 const server = http.createServer(app);
 const port = 3000;
-
 server.listen(port);
-console.debug('Server listening on port ' + port);
+console.debug('Server start at port ' + port);
