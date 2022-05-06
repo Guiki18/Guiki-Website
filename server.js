d01be7d71ab69b3e39299b862e31be1a060d64e1
FoXZillaPT17 <>
2022-05-06 13:57:40 +0000

    Server improvement

--- server.js
+++ server.js
@@ -3,33 +3,34 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const moment = require('moment');
+
+todayDate = new String(moment().format("L"));
+todayDate = todayDate.replace("/", "-");
+todayDate = todayDate.replace("/", "-");
+
 require('better-logging')(console, {
 	format: ctx => `${ctx.time24} ${ctx.date} ${ctx.type} | ${ctx.msg}`,
-	saveToFile: `${__dirname}/logs/${moment().format('L').replaceAll("/", "-")}.log`,
+	saveToFile: `${__dirname}/logs/${todayDate}.log`,
 	logLevels: {
 		debug: true,
 	}
 });
 const { lookup } = require('geoip-lite');
+var requestIp = require('request-ip');
 
 app.use(express.json());
 app.use(express.static("express"));
-
 app.use('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/web/'));
-	const ip = req.socket.remoteAddress;
-	const lookupIp = lookup(ip);
+  var clientIp = requestIp.getClientIp(req);
 
 	const jsonRequestInformation = {
 		"method": req.method,
 		"url": req.url,
 		"ip": {
-			"ip": ip,
-			"lookupIp": lookupIp
+			"ip": clientIp,
 		},
-
 	}
-
 	console.log("Received a Request : \n" + JSON.stringify(jsonRequestInformation, null, '\t'));
 });
 
